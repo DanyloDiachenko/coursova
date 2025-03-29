99d3d1f26c2a96bf2d6fa292e6a29d0fe22310de
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-03-29 23:57:46 +0200

    _

--- src/components/SortProcess/index.tsx
+++ src/components/SortProcess/index.tsx
@@ -1,13 +1,14 @@
-import { useState } from "react";
 import { Button } from "../Button";
 import { SortProcessProps } from "./SortProcess.props";
 
-export const SortingModal = ({ isOpened, onCloseClick }: SortProcessProps) => {
+export const SortProcess = ({ isOpened, onCloseClick }: SortProcessProps) => {
     return true ? (
         <div className="fixed inset-0 flex items-center justify-center bg-black/50">
             <div className="bg-white rounded-2xl p-6 shadow-xl w-[400px] relative">
                 <div className="flex justify-between items-center mb-4">
-                    <h2 className="text-xl font-semibold">Sorting Animation</h2>
+                    <h2 className="text-xl font-semibold">
+                        Анімація сортування
+                    </h2>
                     <button
                         onClick={onCloseClick}
                         className="text-gray-500 hover:text-gray-700"
@@ -19,7 +20,7 @@ export const SortingModal = ({ isOpened, onCloseClick }: SortProcessProps) => {
                     <p className="text-gray-400">Тут будет анимация</p>
                 </div>
                 <div className="mt-6 flex justify-end">
-                    <Button onClick={onCloseClick}>Skip</Button>
+                    <Button onClick={onCloseClick}>Пропустити</Button>
                 </div>
             </div>
         </div>
